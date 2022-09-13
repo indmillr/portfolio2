2553bd1c0a4891e0dfae0a83eb954c618cfd28ff
Ian Miller <theiandmiller@gmail.com>
2022-09-13 06:20:30 -0500

    feat: Header Image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,3 +1,7 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
 export const Banner = () => {
    return (
       <section className="banner" id="home">
@@ -7,7 +11,10 @@ export const Banner = () => {
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>{'Hi I'm webcoded}<span className="wrap">web developer</span></h1>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <button onClick={() => console.log('connect')}>Let's connect </button>
+                  <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
+               </Col>
+               <Col xs={12} md={6} xl={5}>
+                  <img src={headerImg} alt="Header Img" />
                </Col>
             </Row>
          </Container>
