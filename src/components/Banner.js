05089f1db989a502d3237392b734667ea595211f
Ian Miller <theiandmiller@gmail.com>
2022-09-13 06:37:44 -0500

    feat: Text Animation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,42 +1,47 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export const Banner = () => {
    const [loopNum, setLoopNum] = useState(0);
    const [isDeleting, setIsDeleting] = useState(false);
    const toRotate = ["Development", "Design", "UI/UX"];
-   const [text, setText] = useState('');
+   const [text, setText] = useState("");
+   /* Time between each letter */
    const [delta, setDelta] = useState(300 - Math.random() * 100);
    const period = 2000;
 
    useEffect(() => {
       let ticker = setInterval(() => {
          tick();
-      }, delta)
+      }, delta);
 
-      return () => { clearInterval(ticker)};
-   }, [text])    
+      return () => {
+         clearInterval(ticker);
+      };
+   }, [text]);
 
-   const tick() => {
+   const tick = () => {
       let i = loopNum % toRotate.length;
       let fullText = toRotate[i];
-      let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+      let updatedText = isDeleting
+         ? fullText.substring(0, text.length - 1)
+         : fullText.substring(0, text.length + 1);
 
       setText(updatedText);
 
       if (isDeleting) {
-         setDelta(prevDelta => prevDelta /2)
+         setDelta((prevDelta) => prevDelta / 2);
       }
 
       if (!isDeleting && updatedText === fullText) {
-         setIfDeleting(true);
+         setIsDeleting(true);
          setDelta(period);
-      } else if (isDeleting && updatedText === '') {
+      } else if (isDeleting && updatedText === "") {
          setIsDeleting(false);
-         setLoopNum(loopNum +1);
-         setDelta(500);
+         setLoopNum(loopNum + 1);
+         setDelta(200);
       }
    };
 
@@ -46,9 +51,20 @@ export const Banner = () => {
             <Row className="align-items-center">
                <Col xs={12} md={6} xl={7}>
                   <span className="tagline">Welcome to my Portfolio</span>
-                  <h1>{'Hi I am webcoded'}<span className="wrap">{text}</span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
+                  <h1>
+                     {"Hi I am webcoded "}
+                     <span className="wrap">{text}</span>
+                  </h1>
+                  <p>
+                     Lorem Ipsum is simply dummy text of the printing and
+                     typesetting industry. Lorem Ipsum has been the industry's
+                     standard dummy text ever since the 1500s, when an unknown
+                     printer took a galley of type and scrambled it to make a
+                     type specimen book.
+                  </p>
+                  <button onClick={() => console.log("connect")}>
+                     Let's connect <ArrowRightCircle size={25} />
+                  </button>
                </Col>
                <Col xs={12} md={6} xl={5}>
                   <img src={headerImg} alt="Header Img" />
